672acbcfdfafc2fd0610c1a3693040f10e7e084c
happyyj6 <yuwls1102@naver.com>
2022-05-01 00:04:44 +0900

    gittest3

--- app/src/main/java/com/capstone/studysignal/ui/main/MainActivity.kt
+++ app/src/main/java/com/capstone/studysignal/ui/main/MainActivity.kt
@@ -5,10 +5,18 @@ import android.os.Bundle
 import com.capstone.studysignal.R
 
 class MainActivity : AppCompatActivity() {
+    /**
+     *
+     * 1. 뷰바인딩으로 xml의 id를 연결해서 쓸 수 있게 만든다.
+     * 2. bottomView의 id 값을 얻어서, 클릭 리스너를 만든다.
+     * 3. 클릭 리스너에서 콜백(함수)에서 position에 맞게 해당하는 fragment를 실행시킨다.
+     *
+     */
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         // git test
         // second git test
     }
