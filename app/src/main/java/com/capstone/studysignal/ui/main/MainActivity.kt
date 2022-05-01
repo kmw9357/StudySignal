0b7d4bb92f8d7c9bd6d22cc35e2d167a4c4b6316
happyyj6 <yuwls1102@naver.com>
2022-05-01 20:13:00 +0900

    메인 이넘 클래스 정의, 리스너 연결

--- app/src/main/java/com/capstone/studysignal/ui/main/MainActivity.kt
+++ app/src/main/java/com/capstone/studysignal/ui/main/MainActivity.kt
@@ -2,8 +2,11 @@ package com.capstone.studysignal.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.capstone.studysignal.R
+import com.capstone.studysignal.databinding.ActivityMainBinding
 
+// 생명주기 = lifeCycle
 class MainActivity : AppCompatActivity() {
     /**
      *
@@ -12,13 +15,35 @@ class MainActivity : AppCompatActivity() {
      * 3. 클릭 리스너에서 콜백(함수)에서 position에 맞게 해당하는 fragment를 실행시킨다.
      *
      */
+    lateinit var binding : ActivityMainBinding
 
+    enum class MENU(val value: Int) {
+        TODO(0),
+        CALENDER(1),
+        STATISTICS(2),
+        STUDY(3),
+        SETTING(4)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // git test
-        // second git test
-        // test
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bvMain.setOnItemSelectedListener { position ->
+            Log.d("happy","position : $position")
+            if(position == MENU.TODO.value) {
+                Log.d("happy","create todo fragment ")
+            } else if(position == MENU.CALENDER.value) {
+                Log.d("happy","create calendar fragment ")
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
